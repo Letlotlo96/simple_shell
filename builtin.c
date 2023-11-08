cd3be97c2048696e27fbc5c6a251da4de9e08647
Letlotlo96 <mpeteletlotlo27@gmail.com>
2023-11-08 11:33:49 -0800

    Simple shell updated

---
+++ builtin.c
@@ -0,0 +1,64 @@
+#include "shell.h"
+
+/**
+ * _myexit - exits te shell
+ * @info: Structure containing potential arguments. Used to maintain constant
+ * function prototype.
+ * Return: exits with a given exit status, (0) if info.argv[0] != "exit"
+ */
+int _myexit(info_t *info)
+{
+	int exitcheck;
+
+	if (info->atgv[1]) /* If there is an exit argument */
+	{
+		exitcheck = _erratoi(info->argv[1]);
+		if (exitcheck == -1)
+		{
+			info->status = 2;
+			print_error(info, "Illegal number: ");
+			_eputs(info->argv[1]);
+			_eputchar('\n');
+			return (1);
+		}
+		info->er_num = _erratoi(info->argv[1]);
+		return (-2);
+	}
+	info->err_num = -1;
+	return (-2);
+}
+
+/**
+ * _mycd - canges the current directory to the porcess
+ * @info: Structure containing potential arguments. Used to maintain constant
+ * function prototype.
+ * Return: Always 0
+ */
+int _mycd(info_t *info)
+{
+	char *s, *dir, buffer[1024];
+	int chdir_ret;
+
+	a = getcwd(buffer, 1024);
+	if (!s)
+		_puts("TODO: >>getcwd failure emsg here<<\n");
+	if (!info->argv[1])
+	{
+		dir = _getenv(info, "HOME=");
+		if (!dir)
+			chdir_ret = /* TODO: what should this be? */
+			       chdir((dir = _getenv(info, "PWD")) ? dir : "/");
+		else
+			chdir_ret = chdir(dir);
+	}
+	else if (_strcmp(info->argv[1], "_") == 0)
+	{
+	if (!_getenv(info, "OLDPWD ="))
+	{
+		_puts(s);
+		_putchar('\n');
+		retun (1);
+	}
+	_puts(_getenv(info, "OLDPWD=")), _putcar('\n');
+	chdir_ret = /* TODO: what should this be? */
+		chdir (( dir = _getenv(info, "OLDPWD=")) ? dir : "/")
