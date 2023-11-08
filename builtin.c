54b28600c4f2317746aa789ce4a2eccfda62ab68
Letlotlo96 <mpeteletlotlo27@gmail.com>
2023-11-08 12:36:04 -0800

    Simple shell updated

--- builtin.c
+++ builtin.c
@@ -10,7 +10,7 @@ int _myexit(info_t *info)
 {
 	int exitcheck;
 
-	if (info->atgv[1]) /* If there is an exit argument */
+	if (info->argv[1]) /* If there is an exit argument */
 	{
 		exitcheck = _erratoi(info->argv[1]);
 		if (exitcheck == -1)
@@ -29,7 +29,7 @@ int _myexit(info_t *info)
 }
 
 /**
- * _mycd - canges the current directory to the porcess
+ * _mycd - changes the current directory to the porcess
  * @info: Structure containing potential arguments. Used to maintain constant
  * function prototype.
  * Return: Always 0
@@ -53,12 +53,44 @@ int _mycd(info_t *info)
 	}
 	else if (_strcmp(info->argv[1], "_") == 0)
 	{
-	if (!_getenv(info, "OLDPWD ="))
-	{
-		_puts(s);
-		_putchar('\n');
-		retun (1);
+		if (!_getenv(info, "OLDPWD ="))
+		{
+			_puts(s);
+			_putchar('\n');
+			retun (1);
+		}
+		_puts(_getenv(info, "OLDPWD=")), _putcar('\n');
+		chdir_ret = /* TODO: what should this be? */
+			chdir (( dir = _getenv(info, "OLDPWD=")) ? dir : "/");
 	}
-	_puts(_getenv(info, "OLDPWD=")), _putcar('\n');
-	chdir_ret = /* TODO: what should this be? */
-		chdir (( dir = _getenv(info, "OLDPWD=")) ? dir : "/")
+	else
+		chdir_ret = chdir(info->argv[1]);
+	if (chdir_ret == -1)
+	{
+		print_error(info, "can't cd to ");
+		_eputs(info->argv[1]), _eputchar('\n');
+	}
+	else
+	{
+		setenv(info, "OLDPWD", _getenv(info, "PWD="));
+		_setenv(info, "PWD", getcwd(buffer, 1024));
+	}
+	return (0);
+}
+
+/**
+ * _myhelp - changes the current directory of the process
+ * @info: Structure containing potential arguments. Used to maintain constant
+ * function prototype.
+ * Return: Always 0.
+ */
+int _myhelp(info_t *info)
+{
+	char **arg_array;
+
+	arg_array = info->argv;
+	_puts("help call works. Function not yet impelemented \n");
+	if (0)
+		_puts(*arg_array); /* temp att_unsused workaround */
+	return (0);
+}
